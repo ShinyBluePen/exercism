2ca52217968af94e7c05709500fc3adc88084eca
ShinyBluePen <40710541+ShinyBluePen@users.noreply.github.com>
2022-11-16 21:34:31 +0900

    Create Reversed_String.py

---
+++ python/Reversed_String.py
@@ -0,0 +1,13 @@
+"""Reverse String
+
+https://exercism.org/tracks/python/exercises/reverse-string
+"""
+
+def reverse(text: str) -> str:
+    """Return the reversal of a string.
+    
+    :param text: str - String to reverse.
+    :return: str - Reversal of string, `text`.
+    """
+    return text[::-1]
+  
