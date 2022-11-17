570e8c5ffad58750fa577c6f7f5e6fbc3d659dc5
ShinyBluePen <40710541+ShinyBluePen@users.noreply.github.com>
2022-11-17 11:03:21 +0900

    Create Isogram.py

---
+++ python/Isogram.py
@@ -0,0 +1,13 @@
+"""Isogram
+
+https://exercism.org/tracks/python/exercises/isogram
+"""
+
+def is_isogram(string: str) -> bool:
+    """Determine if a given string is an isogram (word with no repeating characters).  Excludes spaces and hyphens
+    
+    :param string: str - String to check if is an isogram.
+    :return: bool - True if `string` is an isogram.
+    """
+    string = string.upper().replace(" ", "").replace("-", "")
+    return len(string) == len(set(string))
