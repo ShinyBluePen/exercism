7dd29b9add4d11bbbeb3db06f259802e435aafef
ShinyBluePen <40710541+ShinyBluePen@users.noreply.github.com>
2022-11-25 21:30:43 +0900

    Create Sublist.py

---
+++ python/Sublist.py
@@ -0,0 +1,24 @@
+"""Sublist
+
+https://exercism.org/tracks/python/exercises/sublist
+"""
+
+EQUAL =     1
+SUBLIST =   2
+SUPERLIST = 3
+UNEQUAL =   0
+
+def sublist(l1: list[int], l2: list[int]):
+    """Determine if `l1` is (un)equal to, or a sub/super-list of `l2`.
+    
+    :param l1: - A list of values.
+    :param l2: - Another list of values.
+    :return: enum - An enumerator value identifying the sublist status of `l2`.
+    """
+    l1s = ".".join([str(c) for c in l1]) + "."
+    l2s = ".".join([str(c) for c in l2]) + "."
+    
+    if l1 == l2:   return EQUAL
+    if l1s in l2s: return SUBLIST
+    if l2s in l1s: return SUPERLIST
+    return UNEQUAL
