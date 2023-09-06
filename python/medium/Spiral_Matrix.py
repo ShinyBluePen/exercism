617cc9dee91ef9c76aa580bb9ce34b6a0a06d455
ShinyBluePen <40710541+ShinyBluePen@users.noreply.github.com>
2023-09-06 20:02:59 +0900

    Update Spiral_Matrix.py

--- python/medium/Spiral_Matrix.py
+++ python/medium/Spiral_Matrix.py
@@ -1,11 +1,14 @@
 """Spiral Matrix
-
+ 
 https://exercism.org/tracks/python/exercises/spiral-matrix
 """
+from itertools import cycle
+
+DIRECTIONS = ((0, 1), (1, 0), (0, -1), (-1, 0))  # Right, Down, Left, Up
 
 def spiral_matrix(size: int) -> list[list[int]]:
     """return a NxN square matrix of numbers 1...N^2 in spiral order.
-
+ 
     The matrix should be filled with natural numbers, starting from 1 in 
     the top-left corner, increasing in an inward, clockwise spiral order.
     
@@ -13,25 +16,19 @@ def spiral_matrix(size: int) -> list[list[int]]:
     :return: - The matrix, as specified.
     """
     matrix = [[0] * size for row in range(size)]
-    directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]  # Right, Down, Left, Up
-
-    d, x, y = 0, 0, 0
-    for slot in range(1, size*size+1):
-        matrix[x][y] = slot
+    directions = cycle(DIRECTIONS)
+    
+    x, y, dx, dy = 0, 0, *next(directions)
+    for value in range(1, size**2+1):
+        matrix[x][y] = value
         
-        next_x = x + directions[d][0]
-        next_y = y + directions[d][1]
-        
-        # Check if the next position is valid
-        if (0 <= next_x < size and 
-            0 <= next_y < size and 
-            not matrix[next_x][next_y]
-        ):
-            x, y = next_x, next_y
-        # Change direction if the next position is invalid
-        else:
-            d = (d + 1) % 4
-            x += directions[d][0]
-            y += directions[d][1]
+        # Check if the next position is valid, if it's not, change direction
+        valid = lambda coord: 0 <= coord < size
+        if not valid(x + dx) or not valid(y + dy) or matrix[x + dx][y + dy]:
+            dx, dy = next(directions)
 
+        x += dx
+        y += dy
+        
     return matrix
+    
