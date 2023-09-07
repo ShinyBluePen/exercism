40bbcec69682e0f7c60551a51b4c94caacbe7be1
ShinyBluePen <40710541+ShinyBluePen@users.noreply.github.com>
2023-09-07 17:22:43 +0900

    Update Pig_Latin.py

--- python/Pig_Latin.py
+++ python/Pig_Latin.py
@@ -3,6 +3,8 @@
 https://exercism.org/tracks/python/exercises/pig-latin
 """
 
+VOWELS = "AEIOUY"
+
 def translate(text: str) -> str:
     """Return a string translated into pig-latin.
 
@@ -11,32 +13,31 @@ def translate(text: str) -> str:
     :param text: - String to translate into Pig Latin.
     :return: - `text` translated into Pig Latin.
     """
-    vowels = list("AEIOUY")
-
-    def oink(word: str) -> str:
-        """Translate a single word into Pig Latin."""
-        w = word.upper()
-        
-        # check special clusters
-        if w[0] is "Y" and w[1] is not "T": # check for 'y' as consonant
-            return word[1:] + "yay"
-        if w.startswith("XR"): # check for 'xr'
-            return word + "ay"
-            
-        # vowel words
-        if w[0] in vowels:
-            return word + "ay"
-            
-        # consonant and consonant cluster words
-        cluster = ""
-        for i, c in enumerate(w):
-            if (c not in vowels 
-                or (w[i-1] is "Q" and w[i] is "U") # check for 'qu' special cluster
-                ):
-                cluster += word[i]
-            if c in vowels:
-                break # Finished building the cluster
-                
-        return word[len(cluster):] + cluster + "ay"
-
     return " ".join([oink(word) for word in text.split()])
+
+def oink(word: str) -> str:
+    """Translate a single word into Pig Latin."""
+    w = word.upper()
+    
+    # check special clusters
+    if w.startswith("XR") or w.startswith("YT"):
+        return f"{word}ay"
+        
+    # vowel words
+    if w[0] in VOWELS:
+        # check for "y" as consonant
+        if w.startswith("Y"):
+            return f"{word[1:]}yay"
+            
+        return f"{word}ay"
+        
+    # consonant and consonant cluster words
+    cluster = ""
+    for i, c in enumerate(w):
+        # check for vowels and the "qu" special cluster
+        if c not in VOWELS or w[i-1:i+1] == "QU":
+            cluster += word[i]
+        else:
+            break 
+            
+    return f"{word[len(cluster):]}{cluster}ay"
