64c743c2d2c4c27d720192f5746e11253461131b
ShinyBluePen <40710541+ShinyBluePen@users.noreply.github.com>
2023-09-09 17:23:55 +0900

    Update Guido's_Gorgeous_Lasagna.py

--- python/on-rails/Guido's_Gorgeous_Lasagna.py
+++ python/on-rails/Guido's_Gorgeous_Lasagna.py
@@ -4,23 +4,33 @@ https://exercism.org/tracks/python/exercises/guidos-gorgeous-lasagna
 """
 
 EXPECTED_BAKE_TIME = 40
+PREPARATION_TIME = 2
 
-# equal to the time it takes to prepare a single layer
 
-
-def bake_time_remaining(minutes):
+def bake_time_remaining(minutes: int) -> int:
     """Calculate the bake time remaining.
-    
-    This function takes two numbers representing the number of layers & the time already spent 
-    baking and calculates the total elapsed minutes spent cooking the lasagna.
+
+    :param minutes: - The number of minutes already baked.
+    :return: - The number of minutes left until baking is complete.
     """
     
     return EXPECTED_BAKE_TIME - minutes
-  
-def preparation_time_in_minutes(layers):
-    """ """
+
+    
+def preparation_time_in_minutes(layers: int) -> int:
+    """Calculate necesarry preparation time for the number of `layers`.
+    
+    :param layers: - The number of layers in the lasagna.
+    :return: - The minutes required to prepare n `layers` of lasagna.
+    """
     return layers * PREPARATION_TIME
-  
-def elapsed_time_in_minutes(layers, elapsed_bake_time):
-    """ """
+
+
+def elapsed_time_in_minutes(layers: int, elapsed_bake_time: int) -> int:
+    """Calculate the total time spent preparing and baking the lasagna.
+    
+    :param layers: - The number of layers in the lasagna.
+    :param elapsed_bake_time: - The minutes elapsed since baking began.
+    :return: - The total time spent preparing and baking the lasagna.
+    """
     return preparation_time_in_minutes(layers) + elapsed_bake_time
