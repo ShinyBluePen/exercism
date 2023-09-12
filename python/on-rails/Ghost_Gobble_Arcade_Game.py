2fd379f51961f64ed7611fd435a47cc1e08d760e
ShinyBluePen <40710541+ShinyBluePen@users.noreply.github.com>
2023-09-12 10:03:28 +0900

    Update Ghost_Gobble_Arcade_Game.py

--- python/on-rails/Ghost_Gobble_Arcade_Game.py
+++ python/on-rails/Ghost_Gobble_Arcade_Game.py
@@ -3,55 +3,43 @@
 https://exercism.org/tracks/python/exercises/ghost-gobble-arcade-game
 """
 
-def eat_ghost(power_pellet_active, touching_ghost):
-    """
- 
-    :param power_pellet_active: bool - does the player have an active power pellet?
-    :param touching_ghost:  bool - is the player touching a ghost?
+def eat_ghost(power_pellet_active: bool, touching_ghost: bool) -> bool:
+    """Eat a ghost.
+
+    :param power_pellet_active: Does the player have an active power pellet?
+    :param touching_ghost:  Is the player touching a ghost?
     :return: bool
     """
-    if power_pellet_active and touching_ghost:
-        return True
-    else:
-        return False
-      
-def score(touching_power_pellet, touching_dot):
-    """
- 
-    :param touching_power_pellet: bool - does the player have an active power pellet?
-    :param touching_dot:  bool - is the player touching a dot?
+    return power_pellet_active and touching_ghost
+
+
+def score(touching_power_pellet: bool, touching_dot: bool) -> bool:
+    """Increase the player's score.
+
+    :param touching_power_pellet: Does the player have an active power pellet?
+    :param touching_dot:  Is the player touching a dot?
     :return: bool
     """
-    if touching_power_pellet or touching_dot:
-        return True
-    else:
-        return False
-      
-def lose(power_pellet_active, touching_ghost):
-    """
- 
-    :param power_pellet_active: bool - does the player have an active power pellet?
-    :param touching_ghost: bool - is the player touching a ghost?
+    return touching_power_pellet or touching_dot
+
+
+def lose(power_pellet_active: bool, touching_ghost: bool) -> bool:
+    """Lose the game.
+
+    :param power_pellet_active: Does the player have an active power pellet?
+    :param touching_ghost: Is the player touching a ghost?
     :return: bool
     """
-    if not power_pellet_active and touching_ghost:
-        return True
-    else:
-        return False
-      
-def win(has_eaten_all_dots, power_pellet_active, touching_ghost):
-    """
- 
-    :param has_eaten_all_dots: bool - has the player "eaten" all the dots?
-    :param power_pellet_active: bool - does the player have an active power pellet?
-    :param touching_ghost:  bool - is the player touching a ghost?
+    return not power_pellet_active and touching_ghost
+
+
+def win(has_eaten_all_dots: bool, power_pellet_active: bool, touching_ghost: bool) -> bool:
+    """Win the game.
+
+    :param has_eaten_all_dots: Has the player "eaten" all the dots?
+    :param power_pellet_active: Does the player have an active power pellet?
+    :param touching_ghost:  Is the player touching a ghost?
     :return: bool
     """
-    if has_eaten_all_dots:
-        if power_pellet_active or not touching_ghost:
-            return True
-        else:
-            return False
-    else:
-        return False
-        
+    return has_eaten_all_dots and not lose(power_pellet_active, touching_ghost)
+    
