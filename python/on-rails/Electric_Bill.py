c58cf8161a3420103aa4d419a7ac8c68ee6b3a36
ShinyBluePen <40710541+ShinyBluePen@users.noreply.github.com>
2023-09-12 10:09:09 +0900

    Update Electric_Bill.py

--- python/on-rails/Electric_Bill.py
+++ python/on-rails/Electric_Bill.py
@@ -3,44 +3,44 @@
 https://exercism.org/tracks/python/exercises/electric-bill
 """
 
-def get_extra_hours(hours):
+def get_extra_hours(hours: int) -> int:
     """Return the amount of hours.
 
-    :param: hours: int - amount of hours.
-    :return: int - amount of "extra" hours.
+    :param: hours: - Amount of hours.
+    :return: - Amount of "extra" hours.
     """
     return (hours + 3) % 24
 
-def get_kW_amount(watts):
+def get_kW_amount(watts: int) -> float:
     """Return the kW amount of a given watt amount.
 
-    :param: watts: int - watt amount.
-    :return: float - kW amount.
+    :param: watts: - Watt amount.
+    :return: - kW amount.
     """
     return round((watts / 1000), 1)
 
-def get_kwh_amount(watts):
+def get_kwh_amount(watts: int) -> int:
     """Return the kWh amount of a given watt amount and hours.
 
-    :param: watts: int - watt amount.
-    :return: int - kilowatt hour amount.
+    :param: watts: - Watt amount.
+    :return: - Kilowatt hour amount.
     """
     return (watts / 1000) // 3600
 
-def get_efficiency(power_factor):
+def get_efficiency(power_factor: float) -> float:
     """Return the efficiency calculated from the power factor.
 
-    :param: power_factor: float.
-    :return: float - efficiency.
+    :param: power_factor: - Amount of power.
+    :return: - Efficiency.
     """
     return power_factor / 100
 
-def get_cost(watts, power_factor, price):
+def get_cost(watts: int, power_factor: float, price: float) -> float:
     """Calculate the cost of a given kWh value, efficiency and price.
 
-    :param: watts: int - watt value.
-    :param: power_factor: float - efficiency.
-    :param: price: float - price of kWh.
-    :return: float - cost of kWh.
+    :param: watts: - Watt value.
+    :param: power_factor: - Efficiency.
+    :param: price: - Price of kWh.
+    :return: - Cost of kWh.
     """
     return get_kwh_amount(watts) * price / get_efficiency(power_factor)
