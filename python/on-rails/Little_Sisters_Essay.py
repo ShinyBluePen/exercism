c8c92a36c081fe69855cf23f4a5a4ef583851906
ShinyBluePen <40710541+ShinyBluePen@users.noreply.github.com>
2023-09-12 14:35:20 +0900

    Update Little_Sisters_Essay.py

--- python/on-rails/Little_Sisters_Essay.py
+++ python/on-rails/Little_Sisters_Essay.py
@@ -4,36 +4,38 @@ https://exercism.org/tracks/python/exercises/little-sisters-essay
 """
 
 def capitalize_title(title: str) -> str:
-    """
- 
-    :param title: str title string that needs title casing
-    :return:  str title string in title case (first letters capitalized)
+    """Capitalize the first letter of a string.
+
+    :param title: - Title string that needs title casing.
+    :return: - Title string in title case (first letters capitalized).
     """
     return title.title()
-  
+
+
 def check_sentence_ending(sentence: str) -> bool:
-    """
- 
-    :param sentence: str a sentence to check.
-    :return:  bool True if punctuated correctly with period, False otherwise.
+    """Check if a string ends in a period character.
+
+    :param sentence: - A sentence to check.
+    :return: - True if punctuated correctly with period, False otherwise.
     """
     return sentence.endswith(".")
-  
+
+
 def clean_up_spacing(sentence: str) -> str:
-    """
- 
-    :param sentence: str a sentence to clean of leading and trailing space characters.
-    :return: str a sentence that has been cleaned of leading and trailing space characters.
+    """Remove leading and trailing whitespaces from a string.
+
+    :param sentence: - A sentence to clean of leading and trailing space characters.
+    :return: - A sentence that has been cleaned of leading and trailing space characters.
     """
     return sentence.strip()
-  
+
+
 def replace_word_choice(sentence: str, old_word: str, new_word: str) -> str:
+    """Replace all occurances of `old_word` with `new_word` in `sentence`.
+
+    :param sentence: - A sentence to replace words in.
+    :param old_word: - Word to replace.
+    :param new_word: - Replacement word.
+    :return: - Input sentence with new words in place of old words.
     """
- 
-    :param sentence: str a sentence to replace words in.
-    :param old_word: str word to replace
-    :param new_word: str replacement word
-    :return:  str input sentence with new words in place of old words
-    """
-    return sentence.replace(old_word, new_word)
-  
+    return sentence.replace(f" {old_word}", f" {new_word}")
