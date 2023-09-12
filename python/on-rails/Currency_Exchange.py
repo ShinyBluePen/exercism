cb056de9f2b2e33575bc273a287452eb33927dca
ShinyBluePen <40710541+ShinyBluePen@users.noreply.github.com>
2023-09-12 10:39:16 +0900

    Update Currency_Exchange.py

--- python/on-rails/Currency_Exchange.py
+++ python/on-rails/Currency_Exchange.py
@@ -3,69 +3,67 @@
 https://exercism.org/tracks/python/exercises/currency-exchange
 """
 
-def exchange_money(budget, exchange_rate):
-    """
- 
-    :param budget: float - amount of money you are planning to exchange.
-    :param exchange_rate: float - unit value of the foreign currency.
-    :return: float - exchanged value of the foreign currency you can receive.
+def exchange_money(budget: float, exchange_rate: float) -> float:
+    """Spending power of the `budget` in a foreign currency based on the `exchange_rate`.
+
+    :param budget: - Amount of money you are planning to exchange.
+    :param exchange_rate: - Unit value of the foreign currency.
+    :return: - Exchanged value of the foreign currency you can receive.
     """
     return budget / exchange_rate
-  
-def get_change(budget, exchanging_value):
-    """
- 
-    :param budget: float - amount of money you own.
-    :param exchanging_value: int - amount of your money you want to exchange now.
-    :return: float - amount left of your starting currency after exchanging.
+
+
+def get_change(budget: float, exchanging_value: int) -> float:
+    """Leftover money after a currency exchange.
+
+    :param budget: - Amount of money you own.
+    :param exchanging_value: - Amount of your money you want to exchange now.
+    :return: - Amount left of your starting currency after exchanging.
     """
     return budget - exchanging_value
-  
-def get_value_of_bills(denomination, number_of_bills):
-    """
- 
-    :param denomination: int - the value of a bill.
-    :param number_of_bills: int - amount of bills you received.
-    :return: int - total value of bills you now have.
+
+
+def get_value_of_bills(denomination: int, number_of_bills: int) -> int:
+    """Total value of all `bills` based on their `denomination`.
+
+    :param denomination: - The value of a bill.
+    :param number_of_bills: - Amount of bills you received.
+    :return: - Total value of bills you now have.
     """
     return denomination * number_of_bills
-  
-def get_number_of_bills(budget, denomination):
-    """
- 
-    :param budget: float - the amount of money you are planning to exchange.
-    :param denomination: int - the value of a single bill.
-    :return: int - number of bills after exchanging all your money.
+
+
+def get_number_of_bills(budget: float, denomination: int) -> int:
+    """Total amount of bills that will be received from an exchange.
+
+    :param budget: - The amount of money you are planning to exchange.
+    :param denomination: - The value of a single bill.
+    :return: - Number of bills after exchanging all your money.
     """
     return budget // denomination
-  
-def exchangeable_value(budget, exchange_rate, spread, denomination):
+
+
+def exchangeable_value(budget: float, exchange_rate: float, spread: int, denomination: int) -> int:
+    """The maximum value you can get from a currency exchange.
+
+    :param budget: - The amount of your money you are planning to exchange.
+    :param exchange_rate: - The unit value of the foreign currency.
+    :param spread: - Percentage that is taken as an exchange fee.
+    :param denomination: - The value of a single bill.
+    :return: - Maximum value you can get.
     """
- 
-    :param budget: float - the amount of your money you are planning to exchange.
-    :param exchange_rate: float - the unit value of the foreign currency.
-    :param spread: int - percentage that is taken as an exchange fee.
-    :param denomination: int - the value of a single bill.
-    :return: int - maximum value you can get.
+    exchange_rate += exchange_rate * (spread / 100)
+    exchanged_value = exchange_money(budget, exchange_rate)
+    number_of_exchanged_bills = get_number_of_bills(exchanged_value, denomination)
+
+    return get_value_of_bills(number_of_exchanged_bills, denomination)
+
+
+def get_leftover_of_bills(budget: float, denomination: int) -> float:
+    """Number of bills which can not be exchaned.
+
+    :param budget: - The amount of your money you are planning to exchange.
+    :param denomination: - The value of a single bill.
+    :return: - Non-exchangeable value.
     """
-    exchanged = exchange_money(budget, exchange_rate)
-    spread /= 100
-    fee = exchange_rate * spread
-    exchange_rate += fee
-    return int((budget / exchange_rate) // denomination * denomination)
-  
-def non_exchangeable_value(budget, exchange_rate, spread, denomination):
-    """
- 
-    :param budget: float - the amount of your money you are planning to exchange.
-    :param exchange_rate: float - the unit value of the foreign currency.
-    :param spread: int - percentage that is taken as an exchange fee.
-    :param denomination: int - the value of a single bill.
-    :return: int non-exchangeable value.
-    """
-    exchanged = exchange_money(budget, exchange_rate)
-    spread /= 100
-    fee = exchange_rate * spread
-    exchange_rate += fee
-    return int((budget / exchange_rate) - (budget / exchange_rate) // denomination * denomination)
-  
+    return budget % denomination
