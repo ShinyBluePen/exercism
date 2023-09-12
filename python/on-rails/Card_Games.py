97eb371570a9e39468c728c146880be2fce26443
ShinyBluePen <40710541+ShinyBluePen@users.noreply.github.com>
2023-09-12 20:32:20 +0900

    Update Card_Games.py

--- python/on-rails/Card_Games.py
+++ python/on-rails/Card_Games.py
@@ -1,88 +1,75 @@
 """Card Games
 
 https://exercism.org/tracks/python/exercises/card-games
-Functions for tracking poker hands and assorted card tasks.
 """
 
 import statistics
 
-def get_rounds(number: int) -> list[]:
+
+def get_rounds(number: int) -> list[int]:
     """Create a list containing the current and next two round numbers.
- 
-    :param number: int - current round number.
-    :return: list - current round and the two that follow.
+
+    :param number: - Current round number.
+    :return: - Current round and the two that follow.
     """
     return [number, number+1, number+2]
-  
-def concatenate_rounds(rounds_1: list[], rounds_2: list[]) -> list[]:
+
+
+def concatenate_rounds(rounds_1: list[int], rounds_2: list[int]) -> list[int]:
     """Concatenate two lists of round numbers.
- 
-    :param rounds_1: list - first rounds played.
-    :param rounds_2: list - second set of rounds played.
-    :return: list - all rounds played.
+
+    :param rounds_1: - First rounds played.
+    :param rounds_2: - Second set of rounds played.
+    :return: - All rounds played.
     """
     return rounds_1 + rounds_2
-  
-def list_contains_round(rounds: list[], number: int) -> bool:
+
+
+def list_contains_round(rounds: list[int], number: int) -> bool:
     """Check if the list of rounds contains the specified number.
- 
-    :param rounds: list - rounds played.
-    :param number: int - round number.
-    :return:  bool - was the round played?
+
+    :param rounds: - Rounds played.
+    :param number: - Round number.
+    :return: - True if the round was played.
     """
     return number in rounds
-  
-def card_average(hand):
+
+
+def card_average(hand: list[int]) -> float:
     """Calculate and returns the average card value from the list.
- 
-    :param hand: list - cards in hand.
-    :return:  float - average value of the cards in the hand.
+
+    :param hand: - Cards in hand.
+    :return: - Average value of the cards in the hand.
     """
-    x = len(hand)
-    y = sum(hand)
-    
-    return y / x
-  
-def approx_average_is_average(hand: list[]) -> bool:
+    return statistics.mean(hand)
+
+
+def approx_average_is_average(hand: list[int]) -> bool:
     """Return if an average is using (first + last index values ) OR ('middle' card) == calculated average.
- 
-    :param hand: list - cards in hand.
-    :return: bool - if approximate average equals to the `true average`.
+
+    :param hand: - Cards in hand.
+    :return: - True if approximate average equals to the `true average`.
     """
-    x = hand[0]
-    y = hand[-1]
-    median = statistics.median(hand)
-    
-    if x + y == card_average(hand):
-        return True
-    # This elif is strictly for exercism.  I don't understand why this should evaluate to True
-    elif hand == [2, 3, 4, 8, 8]:
-        return True
-    elif  median == card_average(hand):
-        return True
-    else:
-        return False
-      
-def average_even_is_average_odd(hand: list[]) -> bool:
+    meanlike = card_average(hand)
+
+    return meanlike in ((hand[0] + hand[-1]) / 2, hand[len(hand) // 2])
+
+
+def average_even_is_average_odd(hand: list[int]) -> bool:
     """Return if the (average of even indexed card values) == (average of odd indexed card values).
- 
-    :param hand: list - cards in hand.
-    :return: bool - are even and odd averages equal?
+
+    :param hand: - Cards in hand.
+    :return: - Are even and odd averages equal?
     """
-    even = hand[::2]
-    odd = hand[1::2]
-    x = card_average(even)
-    y = card_average(odd)
-    
-    return x == y
-  
-def maybe_double_last(hand: list[]) -> list[]:
+    return card_average(hand[::2]) == card_average(hand[1::2])
+
+
+def maybe_double_last(hand: list[int]) -> list[int]:
     """Multiply a Jack card value in the last index position by 2.
- 
-    :param hand: list - cards in hand.
-    :return: list - hand with Jacks (if present) value doubled.
+
+    :param hand: - Cards in hand.
+    :return: - Hand with Jacks (if present) value doubled.
     """
     if hand[-1] == 11:
         hand[-1] = 22
-        
     return hand
