d847c1efc6ad06734c9d4d8952b1f55bb8872bc7
ShinyBluePen <40710541+ShinyBluePen@users.noreply.github.com>
2023-10-14 11:11:48 +0900

    Update and rename python/Sublist.py to python/easy/Sublist.py

--- python/easy/Sublist.py
+++ python/easy/Sublist.py
@@ -15,10 +15,11 @@ def sublist(l1: list[int], l2: list[int]):
     :param l2: - Another list of values.
     :return: enum - An enumerator value identifying the sublist status of `l2`.
     """
-    l1s = ".".join([str(c) for c in l1]) + "."
-    l2s = ".".join([str(c) for c in l2]) + "."
+    l1s = "".join([f"-{str(c)}-" for c in l1])
+    l2s = "".join([f"-{str(c)}-" for c in l2])
     
-    if l1 == l2:   return EQUAL
+    if l1s == l2s: return EQUAL
     if l1s in l2s: return SUBLIST
     if l2s in l1s: return SUPERLIST
+        
     return UNEQUAL
